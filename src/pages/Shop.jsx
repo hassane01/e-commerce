6891b0f0f5f9a56b3adbd692db364a090d32a3df
hassane01 <oubakassehassane7@gmail.com>
2025-05-18 18:46:52 +0100

    initial the shop structure

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
-
+import { MdTableRows } from 'react-icons/md'
+import { RiLayoutGrid2Fill } from 'react-icons/ri'
+import products from '../assets/productslist'
+import ProductCard from '../components/ProductCard'
 const Shop = () => {
   return (
-    <section >
-        
+    <section  className=''>
+        <div className=''>
+            <div>
+                <span>Categorie</span>
+                <ul>
+                    <li>ALL</li>
+                    <li>SMARTPHONE</li>
+                    <li>COMPUTERS</li>
+                    <li>CAMERAS</li>
+                    <li>ON SALES</li>
+                </ul>
+            </div>
+            <div>
+                <div>
+                <RiLayoutGrid2Fill />
+                <MdTableRows />
+                </div>
+                <select name="" id="">
+                    <option value="">low to high</option>
+                    <option value=""> high to low </option>
+                    <option value=""> best seller </option>
+                    <option value=""> New products</option>
+                </select>
+            </div>
+        </div>
+        <div>
+            {products.map((product , index)=>{
+                
+                    <ProductCard product={product} key={index}/>
+                
+
+            })}
+        </div>
     </section>
   )
 }
